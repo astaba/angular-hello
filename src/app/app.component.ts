952056c6edc60c45d1f2825ac90ddd386d1f64c3
Astar <bahouidi99@gmail.com>
2024-07-22 23:01:43 +0100

    Integrate details page
    You now know how to:
    - Use route parameters to pass data to a route
    - Use the routerLink directive to use dynamic data to create a route
    - Use route parameter to retrieve data from the HousingService to display specific housing location information.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { RouterLink, RouterOutlet } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HomeComponent, RouterModule],
+  imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
   template: `
     <main>
       <header class="brand-name">
-        <img
-          class="brand-logo"
-          src="/assets/logo.svg"
-          alt="logo"
-          aria-hidden="true"
-        />
+        <a [routerLink]="['/']">
+          <img
+            class="brand-logo"
+            src="/assets/logo.svg"
+            alt="logo"
+            aria-hidden="true"
+          />
+        </a>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
